2688b89816c363d9a5dfc7d4cad141cc2fa43038
AsheerAF911 <alamfarooqia@gmail.com>
2023-05-17 21:28:23 +0530

    initialized firbase

---
+++ src/Misc/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+
+const config = {
+  apiKey: 'AIzaSyBPrpl9pl_45pfweVVTlMY6r8n2nqPxq9o',
+  authDomain: 'chatterbox-5f17f.firebaseapp.com',
+  projectId: 'chatterbox-5f17f',
+  storageBucket: 'chatterbox-5f17f.appspot.com',
+  messagingSenderId: '384874596336',
+  appId: '1:384874596336:web:07899b3977c4f70a487f35',
+};
+
+const app = firebase.initializeApp(config);
